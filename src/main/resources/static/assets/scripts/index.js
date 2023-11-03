08d294430b72c4ef7de74cf1e8544f0a2fd4e1ef
SantiagoG1993 <gamarras1993@gmail.com>
2023-11-02 22:53:07 -0300

    a

---
+++ src/main/resources/static/assets/scripts/index.js
@@ -0,0 +1,22 @@
+const {createApp} = Vue;
+
+const App = createApp({
+
+    data(){
+        return{
+
+
+        }
+    },
+    created(){
+
+
+
+    },
+    nethods:{
+
+
+
+    }
+});
+App.mount("#app")
