558255f4016a1740f52fe195b6a8d0080bbd4f4d
SantiagoG1993 <gamarras1993@gmail.com>
2023-11-07 01:05:04 -0300

    app conectada a postgres

--- src/main/java/AlmaLibre/eCommerce/ECommerceApplication.java
+++ src/main/java/AlmaLibre/eCommerce/ECommerceApplication.java
@@ -1,7 +1,11 @@
 package AlmaLibre.eCommerce;
 
+import AlmaLibre.eCommerce.models.Client;
+import AlmaLibre.eCommerce.respositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ECommerceApplication {
@@ -9,5 +13,19 @@ public class ECommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args)->{
+
+			Client client1 =new Client("Santiaog","Gamarra","3412475871");
+			clientRepository.save(client1);
+
+
+
+		};
+
+	}
+
 
 }
---
+++ src/main/java/AlmaLibre/eCommerce/models/Client.java
@@ -0,0 +1,54 @@
+package AlmaLibre.eCommerce.models;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName, lastName,phone;
+
+    public Client() {
+    }
+
+    public Client(String firstName, String lastName, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
