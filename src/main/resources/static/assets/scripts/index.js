97709b49c7093b7c1a9e448beac39ff02ce2e248
SantiagoG1993 <gamarras1993@gmail.com>
2023-11-09 15:30:21 -0300

    agregado loguin form y footer

--- src/main/resources/static/assets/scripts/index.js
+++ src/main/resources/static/assets/scripts/index.js
@@ -4,15 +4,38 @@ const App = createApp({
 
     data(){
         return{
+            timeOut:null
 
         }
     },
     created(){
 
-
-
+       
     },
-    nethods:{
+    methods:{
+        
+
+        showProducts(){
+            const item = document.querySelector(".menu_productos")
+            item.classList.add("show--productos")
+            clearTimeout(timeOut)
+        },
+        closeProducts(){
+            timeOut=setTimeout(() => {
+                const item = document.querySelector(".menu_productos");
+                item.classList.remove("show--productos");
+            },300)
+        },
+        showLogin(){
+            const item = document.querySelector(".user_modal")
+            item.classList.add("show--login")
+            document.body.style.overflow = "hidden";
+        },
+        closeLogin(){
+            const item = document.querySelector(".user_modal")
+            item.classList.remove("show--login")  
+            document.body.style.overflow = "";
+        }
 
 
 
