f3a36baf13820a8cfad2be737cae86962185701d
SantiagoG1993 <gamarras1993@gmail.com>
2023-11-23 14:52:35 -0300

    agregue form agregar producto

--- src/main/resources/static/assets/scripts/manager.js
+++ src/main/resources/static/assets/scripts/manager.js
@@ -30,7 +30,15 @@ showMessagesPanel(){
     this.productsTable = true;
     this.imagesPanel = false;
     this.messagesPanel=!this.messagesPanel;
-}
+},
+showAddProduct(){
+    const item = document.querySelector(".add-producto_modal")
+    item.classList.add("show--products--modal")
+},
+closeAddProduct(){
+    const item = document.querySelector(".add-producto_modal")
+    item.classList.remove("show--products--modal")
+},
 
 
     }
